fb9518f3b2dd327b77bf7871b05d74e2d26aa118
Mohamed Al-Shurbagy <114092198+Mohamed-Alshurbagy@users.noreply.github.com>
2024-03-09 20:04:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,21 @@
 // Collecting Document Data
-
 let one = document.querySelector(".one");
-
 let select = document.getElementById("select");
-
 let details = document.querySelector('.details');
-
 let two = document.querySelector(".two");
-
 let title = document.getElementById('title');
-
 let bicon = document.querySelector('.burger-icon');
-
 let wordToWrite = document.getElementById("word");
-
 let input = document.querySelector("input");
-
 let final = document.querySelector(".final");
-
 let time = document.getElementById("time");
-
 let score = document.getElementById("score");
-
 let gameMood = document.getElementById("game-mood");
-
 let start = document.getElementById("start-btn");
-
 let from = document.getElementById("from");
-
 let msg = document.querySelector('.one h3');
-
 let playAgain = document.querySelector('.two p');
-
 let nav = document.querySelector('nav');
-
 let close = document.querySelector('nav div');
 
 // Reset Past Event To False
@@ -96,29 +78,25 @@ let words = [
 ];
 
 // Define Game Levels;
-
 let lvl = {
     Easy: 6,
     Normal: 4,
     Hard: 3,
 };
 
-// Closeing Navbar On Phone, iPad & Tqblets;
-
-close.onclick = function(){
+// Closeing Navbar On Phone, iPad & Tablets;
+close.onclick = function() {
   nav.classList.remove('open')
 };
 
 // Coding Start Game Button;
-
 start.onclick = function(){
-  if(select.value === 'Game Mood'){
+  if (select.value === 'Game Mood') {
     msg.innerHTML = 'Select Mood First!';
     setInterval(()=> {
       msg.innerHTML = 'Select Game Mood and Click Start Play ..!';
     }, 2000);
   }
-  
   else {
     details.remove();
     one.style.display = 'none'
@@ -132,48 +110,44 @@ start.onclick = function(){
 };
 
 // Using The Location Object To Reload The Game While Clicking in Play Again Button;
-
 playAgain.onclick = ()=>location.reload();
 
 // Coding Burger-Icon Button
-
-bicon.onclick = function(){
+bicon.onclick = function() {
   nav.style.display = 'block'
   nav.classList.add('open');
 };
 
 // Creat Random Word To The End User;
-
-function creatWords (){
+function creatWords () {
   let rnd = words[Math.floor(Math.random() * words.length)];
   wordToWrite.innerHTML = rnd;
   let index = words.indexOf(rnd);
   words.splice(index, 1);
-  if(score.innerHTML === from.innerHTML){
+  if (score.innerHTML === from.innerHTML) {
     time.innerHTML = "0";
     wordToWrite.innerHTML = '(You Win )';
     title.innerHTML = 'Congratulations ..';
     input.remove()
     playAgain.style.display = 'block'
-  }else
+  }
+  else
   Play();
 }
 
 // Function To Start Play;
-
-function Play(){
+function Play() {
   time.innerHTML = lvl[select.value];
   let second = setInterval(() => {
     time.innerHTML --;
-    if(input.value ===
-    wordToWrite.innerHTML){
+    if (input.value === wordToWrite.innerHTML) {
       score.innerHTML ++;
       input.value = '';
       wordToWrite.innerHTML = '';
       clearInterval(second);
       creatWords();
     }
-    else if (time.innerHTML === "0" & input.value !== wordToWrite.innerHTML){
+    else if (time.innerHTML === "0" & input.value !== wordToWrite.innerHTML) {
       wordToWrite.innerHTML = 'Game Over !';
       input.remove();
       wordToWrite.classList.add('game-over');
